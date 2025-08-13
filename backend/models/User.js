1c18f286e2d73cfe1dd4551645fce05e721f23e5
yyymaaa <ojukaamy43@gmail.com>
2025-08-13 19:21:10 +0300

    User Model created

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // hashed
+  role: { type: String, enum: ['admin', 'analyst', 'viewer'], default: 'analyst' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
