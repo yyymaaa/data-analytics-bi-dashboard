4f9581ae4986012df2d8b47e0fedcf216908dff1
yyymaaa <ojukaamy43@gmail.com>
2025-08-14 22:10:46 +0300

    Routing for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import Login from "./pages/Login";
 
-function App() {
-  const [message, setMessage] = useState('');
+function Dashboard() {
+  return (
+    <div>
+      <h2>Welcome to the Data Analytics and BI Dashboard</h2>
+      <p>You are now logged in.</p>
+    </div>
+  );
+}
 
+function Home() {
+  const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch('http://localhost:5000/api/health')
-      .then(res => res.json())
-      .then(data => setMessage(data.status))
-      .catch(err => setMessage('Error fetching API'));
+    fetch("http://localhost:5000/api/health")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.status))
+      .catch(() => setMessage("Error fetching API"));
   }, []);
 
   return (
@@ -18,4 +28,18 @@ function App() {
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/*Health check page*/}
+        <Route path = "/" element={<Home />}/>
+        {/*Login page*/}  
+        <Route path = "/dashnoard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+
